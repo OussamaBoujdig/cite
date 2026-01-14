b6376f7178f29ac480d8ed93247437a6a09c3633
OussamaBoujdig <OussamaBoujdig8.com>
2026-01-14 03:35:35 +0100

    Fix scroll to top on navigation - Contact page now scrolls to top

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Solution from './pages/Solution'
 import Contact from './pages/Contact'
+import ScrollToTop from './components/ScrollToTop'
 
 function App() {
   return (
-    <Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/solution" element={<Solution />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
+        <Route path="/solution" element={<Solution />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   )
 }
 
